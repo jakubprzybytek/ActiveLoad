806f0c008816e5ef966b5a25d282ea046a1bf442
Jakub Przybytek <cubanan@gmail.com>
2018-04-16 22:11:38 +0200

    Atmel project init

---
+++ AtmelStudio/Debug/makedep.mk
@@ -0,0 +1,12 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+main.cpp
+
+Peripheral\TWI.cpp
+
+Screen.cpp
+
+SSD1306\SSD1306.cpp
+
